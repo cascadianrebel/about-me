9e060af49a86c5f8315c59cce7b3e05f88d5a4aa
Anthony Green <Anthonydgreen90@gmail.com>
2018-03-06 17:00:44 -0800

    ensure the prompts are displayed

--- js/app.js
+++ js/app.js
@@ -4,3 +4,20 @@
 
 alert( 'NOTICE: If you don\'t like fun websites, leave now.');
 
+var yorn = ' (Please respond by typing either \'yes\' or   \'no\'';
+var userName = prompt ('What is your name?'+ yorn); 
+
+alert ('Welome, ' + userName + ', I\'m Anthony. Let\'s get started!');
+
+//can i insert a button to press to start these questions?
+var userKnows = prompt ('Could you spot Anthony out of a police line-up?' + yorn );
+//possibly add a picture of handsome celebrities to an alert with the text that says "try! Do you see him"
+var seattleLive = prompt ('Have I lived in Seattle for less than 3 years?' + yorn);
+//respond "yorn, I've lived here for 4 years to the day, and I'm still as scared to death of earthquakes"
+var fearQuakes = prompt ('Have I experienced any earthquakes before? ' + yorn);
+//respond "nope but my parents have had two in Madison, Mississippi in the last 4 years. Isn't that 'fracking' awesome?"
+var likeNFL = prompt ('Does Anthony watch the NFL?' + yorn );
+//respond "not unless the Saints are on! WHO DAT!"
+
+//rhetorical question below, must confirm
+alert('Wow, Anthony sounds really cool, right?');

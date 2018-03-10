28a76a02671ed1646df569bc196d76b86f1941e4
Anthony Green <Anthonydgreen90@gmail.com>
2018-03-09 19:51:02 -0800

    fixed array prompts

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-alert('NOTICE: If you don\'t like fun websites, leave now.');
 var counter = 0;
 var yorn = ' (Please respond by typing either \'yes\' or \'no\')';
 var userName = prompt('What is your name?');
@@ -93,25 +92,42 @@ function mathQuestion() {
   var i = 1;
   mathTest = parseInt(mathTest);
   while(i < 4) {
-    if (mathTest === z) {alert('You think you are so smart, don\'t you? +1pt');
+    if (mathTest === z) {
+      alert('You think you are so smart, don\'t you? +1pt');
+      alert('It took you ' + i + ' attempts!');
       break;
-    }else if (mathTest !== z && i < 4){
+    } else if (mathTest !== z && i < 4){
       mathTest = prompt('Let\'s try that again. What is ' + x + '*' + y + '?');
       mathTest = parseInt(mathTest);
       console.log(UserName + ' guessed ' + mathTest);
-      i+;
-    }else (i >= 4 && mathTest !== z);
-    alert('Really? You had four tries. +0pts');
+      i++;
+    }
   }
 }
 mathQuestion();
-console.log('It took you ' + i + ' attempts!');
+
 
 //Q7
-//TODO: 
-var uStates = ['Alaska', 'Connecticut', 'Delaware', 'Hawaii', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Maine', 'Michigan', 'Nebraska', 'New Hampshire', 'New Jersey', 'New York', 'North Dakota', 'Ohio', 'Oklahoma','Pennsylvania', 'South Dakota', 'Vermont'];
-var stateRemain = prompt('Can you guess which states I have NOT yet traveled?');
-var remain = 0 
-while (remain < 7){
-  stateRemain;
+//TODO:
+var correctStateResponses = ['Alaska', 'Connecticut', 'Delaware', 'Hawaii', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Maine', 'Michigan', 'Nebraska', 'New Hampshire', 'New Jersey', 'New York', 'North Dakota', 'Ohio', 'Oklahoma','Pennsylvania', 'South Dakota', 'Vermont'];
+
+var guessesRemaining = 7;
+
+while(guessesRemaining > 0) {
+  var stateResponses = prompt('Which states have I yet to visit?');
+  console.log(guessesRemaining);
+  guessesRemaining--;
+
+  for(var i=0; i < correctStateResponses.length; i++) {
+    if(correctStateResponses[i].toLowerCase() === stateResponses.toLowerCase()){
+      console.log('user guessed ' + stateResponses);
+      alert('Correct! I have not yet visited any of the following states: ' + correctStateResponses);
+      score++;
+      guessesRemaining = -1;
+      break;
+    }
+  }
+  if (guessesRemaining > 0){
+    alert('Nope, been there, done that!');
+  }
 }
